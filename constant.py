cdeec05b7ee11c5054a0517e3d4d44b7c30b8239
Mohammad Dori <mr.dori.dev@gmail.com>
2021-11-04 12:24:14 +0330

    add constant file

---
+++ constant.py
@@ -0,0 +1,13 @@
+"""Project constant variables
+"""
+
+# screen size
+WIDTH = 800
+HEIGHT = 800
+# Number of cells
+COUNT = 20
+# Cell distance
+X_DIST = WIDTH // COUNT
+Y_DIST = HEIGHT // COUNT
+# Time interval between each generation
+INTERVAL_TIME = 0
